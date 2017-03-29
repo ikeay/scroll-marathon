8a9737b11435bffc3e43afca1301f3cca4400de7
Ayaka Ikezawa <ikeay@Ayaka-no-MacBook-Pro.local>
2017-03-29 21:20:35 +0900

    アプリケーション バージョン0.0.2

---
+++ background.js
@@ -0,0 +1,51 @@
+chrome.storage.local.get('smdata', function(data){
+　// 初期値
+  var smdata = {};
+  smdata.sumPx = 0;
+  smdata.startDate = new Date();
+  smdata.startDate = smdata.startDate.toString();
+
+  if (data.smdata == undefined){
+    chrome.storage.local.set({smdata: smdata}, function(){});
+  } else {
+    if(data.smdata.sumPx != undefined){
+    // データが保存されていたら、そのデータを代入
+    smdata.sumPx = data.smdata.sumPx;
+    smdata.startDate = data.smdata.startDate;
+    }
+  }
+  // スクロールイベントが走ったら、データを更新
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.px != 'undefined'){
+      smdata.sumPx += request.px;
+        chrome.storage.local.set({smdata: smdata}, function(){});
+      sendResponse({sum: smdata.sumPx, date: smdata.startDate});
+    }
+  }
+  );
+
+  // タブを更新 or 開いたら実行される
+  chrome.tabs.onUpdated.addListener(function(tabid, info, tab){
+    if(info.status == 'complete'){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabid, {start: 'start', sum: smdata.sumPx, date: smdata.startDate}, function(response) {});
+    });
+    }
+  });
+
+  // データクリア
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log(tab.id);
+    chrome.storage.local.clear(function() {
+      smdata.sumPx = 0;
+      smdata.startDate = new Date();
+      smdata.startDate = smdata.startDate.toString();
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tab.id, {start: 'start', sum: smdata.sumPx, date: smdata.startDate}, function(response) {});
+      });
+    });
+  });
+});
+
+
---
+++ content.js
@@ -0,0 +1,108 @@
+$(function(){
+  var goalPx = 159477367; // 42.195km
+  var lastPx = 0;
+  var pointerX = 0;
+  var pointerY = 0;
+  var stopTimeFlag = false;
+  var timer, stopTime;
+  var startTime, sumPx;
+
+  // スタートしてから今までの時間を計算する
+  function currentRecord(){
+    var nowTime = new Date();
+    var time = nowTime - startTime;
+    var hours = Math.floor(time / (60 * 60 * 1000));
+    var minutes = Math.floor(time / (60 * 1000)) % 60; 
+    var seconds = Math.floor(time / 1000) % 60 % 60;
+    return hours.toString() + ':' + minutes.toString() + ':' + seconds.toString();
+  }
+
+  // ピクセルをメートル法に
+  function pixelToMetric(px){
+    var mm = px * 0.264583;
+    if (mm < 10){
+      return mm.toFixed(1).toString() + 'mm'
+    } else if (mm < 1000){
+      var cm = mm / 10;
+      return  cm.toFixed(2).toString() + 'cm';
+    } else if (mm < 1000000){
+      var m = mm / 1000;
+      return m.toFixed(2).toString() + 'm';
+    } else {
+      var km = mm / 1000000;
+      return km.toFixed(2).toString() + 'km';
+    }
+  }
+  
+  // ログの表示
+  function showLog(){
+    $('#sm-counter').css('position', 'fixed');
+    $('#sm-counter').css('top', pointerY);
+    $('#sm-counter').css('left', pointerX);
+  }
+
+  // マウスが動いたら
+  $(window).mousemove(function(e){
+    pointerX = e.clientX;
+    pointerY = e.clientY;
+    showLog();
+  });
+
+  // 初期化
+  function init(){
+    lastPx = $(window).scrollTop();
+    var distance = pixelToMetric(sumPx);
+    var time = currentRecord(startTime);
+    var div = '<span id="sm-counter"><span id="sm-distance">' + distance + '</span>,<br><span id="sm-time">' + time + '</span></span>';
+    $('#sm-counter').remove();
+    $('body').append(div);
+  }
+
+  function cronItems(){
+    // 100ミリ秒ごとに
+    timer = setInterval(function(){
+      var time = currentRecord();
+      $('#sm-time').text(time);
+    }, 100);
+
+    // スクロールごとに
+    $(window).scroll(function(){  
+      if(sumPx >= goalPx){
+        if(!stopTimeFlag){
+          var time = currentRecord();
+          stopTime = time;
+          stopTimeFlag = true;
+          clearInterval(timer);
+        }
+        $('#sm-counter').html('<span id="sm-distance">Goal!</span>,<br><span id="sm-time">' + stopTime + '</span></span>');
+      } else {
+        var nowPx = $(window).scrollTop();
+        var diff = nowPx - lastPx;
+        if (diff > 0 && diff < 50){
+          chrome.runtime.sendMessage({px: diff}, function(response) {
+            var distance = pixelToMetric(response.sum);
+            $('#sm-distance').text(distance);
+          });
+        }
+        lastPx = nowPx;
+      }
+    });
+  }
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.start == 'start'){
+        startTime = Date.parse(request.date);
+        sumPx = request.sum;
+        init();
+        cronItems(); 
+      }
+    }
+  );
+
+});
+
+
+
+
+
